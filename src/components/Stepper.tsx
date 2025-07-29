80bca11565e30db991cdc1e62da2906dfab70f6f
benny.xiong <xiong.benny@gmail.com>
2025-07-28 20:27:52 -0400

    stepper

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { GoAFormStepper, GoAFormStep } from "@abgov/react-components";
+import { useNavigate } from 'react-router-dom';
+
+const routePaths = [
+  "/",
+  "/citizenship",
+  "/peopleAndAge",
+  "/income",
+  "/location",
+  "/housingOptions"
+];
 
 const Stepper: React.FC = () => {
-    
+    const navigate = useNavigate();
+	const [step, setStep] = useState<number>();
+
+    useEffect(() => {
+        setStep(routePaths.indexOf(location.pathname) + 1)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [navigate]);
+
     return (
-        <GoAFormStepper testId="foo" onChange={() => { }}>
+        <GoAFormStepper testId="foo" step={step} onChange={(i) => navigate(routePaths[i - 1])}>
             <GoAFormStep text="Start"></GoAFormStep>
             <GoAFormStep text="Citizenship"></GoAFormStep>
             <GoAFormStep text="People and ages"></GoAFormStep>
